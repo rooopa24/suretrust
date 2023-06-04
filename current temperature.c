c5cc45fe327d0de0de17899333b9cc246981e466
rooopa24 <132284007+rooopa24@users.noreply.github.com>
2023-06-04 19:20:35 +0530

    Add files via upload

---
+++ current temperature.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float temperature;
+
+    printf("Enter the current temperature in Celsius: ");
+    scanf("%f", &temperature);
+
+    if (temperature > 30.0) {
+        printf("Temperature exceeds safe limits.\n");
+    }
+
+    return 0;
+}
