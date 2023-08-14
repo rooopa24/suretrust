450e2459ccdca86baab28e632f574d924d466351
rooopa24 <132284007+rooopa24@users.noreply.github.com>
2023-08-14 18:54:33 +0530

    Add files via upload

---
+++ blink led/p111.ino
@@ -0,0 +1,15 @@
+int LED_1=5;
+
+void setup()
+ {
+  // put your setup code here, to run once:
+  pinMode(LED_1,  OUTPUT);
+
+}
+
+void loop()
+ {
+  // put your main code here, to run repeatedly:
+digitalWrite(LED_1, HIGH);
+
+}
